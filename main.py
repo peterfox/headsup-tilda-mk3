9366ae3410c39670c4226c05304fcaeab9903d37
Ryan FIeld <Ryan@Ryans-Mac-mini.local>
2016-08-20 22:51:34 +0100

    All about the refactoring

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ ugfx.init()
 buttons.init()
 buttons.disable_menu_reset()
 
+def changePixel(color):
+    pin = machine.Pin("PB13", machine.Pin.OUT)
+    neo = pyb.Neopix(pin)
+    neo.display(color)
+
 def splash():
     changePixel(0x00BFFF)
     ugfx.area(0,0,ugfx.width(),ugfx.height(),ugfx.html_color(0xC390D4))
@@ -45,48 +50,9 @@ def menu():
 
 def loadGame(level):
     if level == 0:
-        ugfx.area(0,0,ugfx.width(),ugfx.height(),ugfx.html_color(0x90C3D4))
-        ugfx.set_default_font(ugfx.FONT_TITLE)
-        ugfx.text(30, 30, "Food: ", 0xFFFF)
-        changePixel(0xFF0000)
-        pyb.delay(1000)
-        ugfx.area(0,0,ugfx.width(),ugfx.height(),ugfx.html_color(0x90C3D4))
-        ugfx.text(30, 30, "Starting in: ", 0xFFFF)
-        changePixel(0xFF6600)
-        #timer1 = pyb.Timer(2)
-        #timer1.init(freq=1)
-        totalTime = 5
-        timer1 = pyb.Timer(2, prescaler=83, period=0x3fffffff)
-        timer1.counter(0)
-        s=ugfx.Style()
-        s.set_background(ugfx.html_color(0x90C3D4))
-        loadingContainer = ugfx.Container(0,30, 320, 30)
-        loadingContainer.show()
-        loadingContainer.area(0,0,320,30,ugfx.html_color(0x90C3D4))
-        loadingContainer.text(0, 0, "Starting in: ", 0xFFFF)
-        ugfx.set_default_font(ugfx.FONT_NAME)
-        loadingContainerCount = ugfx.Container(0,60, 320, 180)
-        loadingContainerCount.show()
-        loadingContainerCount.area(0,0,320,180,ugfx.html_color(0x90C3D4))
+        levelDisplay("Food")        
+        countDown(5)
         
-        while True:
-            currentTime = timer1.counter()
-            count = currentTime / 1000000
-            if count < 5:
-                loadingContainerCount.area(0,0,320,180,ugfx.html_color(0x90C3D4))            
-                dispTime = totalTime - count
-                #ugfx.area(0,0,ugfx.width(),ugfx.height(),ugfx.html_color(0x90C3D4))
-                #ugfx.Label(20,0,300,160," %d " % (dispTime),parent=loadingContainerCount, justification=ugfx.Label.CENTER, style=s)
-                dispTimeInt = int(dispTime)
-                if dispTimeInt%2==0:
-                    changePixel(0xFF0000)
-                    ugfx.text(130,100," %d " % (dispTime),ugfx.RED)
-                else:
-                    changePixel(0xFF6600)
-                    ugfx.text(130,100," %d " % (dispTime),ugfx.ORANGE)
-                pyb.delay(60)
-            else:
-                break
         while True:
             pyb.wfi()
             if buttons.is_triggered("BTN_A"):
@@ -94,12 +60,48 @@ def loadGame(level):
             if buttons.is_triggered("BTN_MENU"):
                 playing = 0 #pyb.hard_reset()
                 break
+                
+def levelDisplay(levelName):
+    ugfx.area(0,0,ugfx.width(),ugfx.height(),ugfx.html_color(0x90C3D4))
+    ugfx.set_default_font(ugfx.FONT_TITLE)
+    ugfx.text(30, 30, "%s: " % (levelName), 0xFFFF)
+    changePixel(0xFF0000)
+    pyb.delay(1000)
+    ugfx.area(0,0,ugfx.width(),ugfx.height(),ugfx.html_color(0x90C3D4))
+                
+def countDown(countTime):
+    timer1 = pyb.Timer(2, prescaler=83, period=0x3fffffff)
+    timer1.counter(0)
+    s=ugfx.Style()
+    s.set_background(ugfx.html_color(0x90C3D4))
+    loadingContainer = ugfx.Container(0,30, 320, 30)
+    loadingContainer.show()
+    loadingContainer.area(0,0,320,30,ugfx.html_color(0x90C3D4))
+    loadingContainer.text(0, 0, "Starting in: ", 0xFFFF)
+    ugfx.set_default_font(ugfx.FONT_NAME)
+    loadingContainerCount = ugfx.Container(0,60, 320, 180)
+    loadingContainerCount.show()
+    loadingContainerCount.area(0,0,320,180,ugfx.html_color(0x90C3D4))
+    while True:
+            currentTime = timer1.counter()
+            count = currentTime / 1000000
+            if count < countTime:
+                loadingContainerCount.area(0,0,320,180,ugfx.html_color(0x90C3D4))            
+                dispTime = countTime - count
+                #ugfx.area(0,0,ugfx.width(),ugfx.height(),ugfx.html_color(0x90C3D4))
+                #ugfx.Label(20,0,300,160," %d " % (dispTime),parent=loadingContainerCount, justification=ugfx.Label.CENTER, style=s)
+                dispTimeInt = int(dispTime)
+                if dispTimeInt%2==0:
+                    changePixel(0xFF6600)
+                    ugfx.text(130,100," %d " % (dispTime),ugfx.ORANGE)
+                else:
+                    changePixel(0xFF0000)
+                    ugfx.text(130,100," %d " % (dispTime),ugfx.RED)
+                pyb.delay(60)
+            else:
+                break
+    return 0
             
-def changePixel(color):
-    pin = machine.Pin("PB13", machine.Pin.OUT)
-    neo = pyb.Neopix(pin)
-    neo.display(color)
-
 playing = 1
 while playing:
     splash()
